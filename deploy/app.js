20fa8473e5ebca097a985184444c906e33637d5f
Jay <lcm000522@gmail.com>
2024-09-16 23:19:22 +0900

    result

--- deploy/app.js
+++ deploy/app.js
@@ -12,23 +12,26 @@ const PORT = 3000; // port 3000
 
 app.set('views', path.join(__dirname, '/static/views'));
 app.set("view engine", "ejs");
+
+
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+// 정적 파일 제공
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 
 app.post('/login', jsonParser, (req, res) => {
     console.log(req.body)
     const { username , password } = req.body
 
-    if (compare('password', password) == true) { // password 값이 password임.
-        // res.send(fs.readFileSync('flag.txt'))
-        res.send('FLAG')
+    if (compare('password', password) == true) { // password 변경해주세요. (password 값이 password임.)
+        res.send('FLAG{flag_falg}') // flag is here!!!!!!
     } else {
         res.send('Wrong!')
     }
 })
-
 
 
 app.listen(PORT, () => { // express run....
