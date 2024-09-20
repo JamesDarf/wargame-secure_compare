92ed8716335cc2d97a4e621502ac98766dfb70eb
Jay <lcm000522@gmail.com>
2024-09-20 23:05:31 +0900

    prevent

--- deploy/app.js
+++ deploy/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
-const compare = require('secure-compare');
+const secureCompare = require('secure-compare');
 const bodyParser = require('body-parser')
 
 
 const app = express();
 const jsonParser = bodyParser.json();
 
+
 const PORT = 3000; // port 3000
+
+
 
 app.set('views', path.join(__dirname, '/static/views'));
 app.set("view engine", "ejs");
@@ -25,15 +28,34 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 app.post('/login', jsonParser, (req, res) => {
     console.log(req.body)
     const { username , password } = req.body
+    const serverPassword = req.headers['x-server-password']
 
-    if (compare('password', password) == true) { // password 변경해주세요. (password 값이 password임.)
-        res.send('FLAG{flag_falg}') // flag is here!!!!!!
-    } else {
-        res.send('Wrong!')
-    }
+    // please, Don't try Brute forcing!!!
+    setTimeout(() => {
+        if (password == serverPassword) {
+            return res.send('Do not hacking!!!!')
+        }
+        if (secureCompare(serverPassword, password) == true) { // password 변경해주세요. (password 값이 password임.)
+            
+        // flag is here!!!!!!
+        try {
+            const flag = fs.readFileSync('flag.txt', {encoding: 'utf8', flag: 'r'})
+            const ip = req.header["x-forwarded-for"] || req.connection.remoteAddress
+            fs.writeFileSync('./server.log', `${ip} | ${password}`, {flags: 'a'})
+            return res.send(`flag is ${flag}`)
+        } catch (error) {
+            console.log(error)
+            return res.send('Internal Server Error')
+        }
+        
+        } else {
+            return res.send('Wrong!')
+        }
+    }, 5000)
 })
 
 
-app.listen(PORT, () => { // express run....
+app.listen(PORT, () => { 
     console.log("Start")
 })
+
